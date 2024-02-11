939b313eb90123f2ba2fc6cfc7093029692d4c70
agnisrini <agni.srinivasan@yahoo.com>
2024-02-11 18:54:13 +0530

    cleaning up code

--- GrievanceRedressal.py
+++ GrievanceRedressal.py
@@ -9,7 +9,6 @@ from langchain.chains import LLMChain
 from langchain_community.llms import OpenAI 
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
 from st_aggrid import AgGrid, GridOptionsBuilder
 
 def initialize_ai_env():
@@ -20,12 +19,12 @@ def initialize_ai_env():
         genai.configure(api_key = os.environ['GOOGLE_API_KEY'])
 
 class grievanceRedressal:
-    def __init__(self,st_obj,inp_query,department):
-        self.st_1=st_obj
-        self.inp_query=''
-        self.department=''
-        self.llm_resp=''
-        self.chat_history=''
+    llm_resp=''
+    chat_history=''
+    inp_query=''
+    department=''
+    def __init__(self,st_obj):
+        self.st_obj=st_obj
     
     def add_bg_from_local(self,image_file):
 
@@ -35,12 +34,12 @@ class grievanceRedressal:
            st.markdown(f"""<style>.stApp {{background-image: url(data:image/{"png"};base64,{encoded_string.decode()});background-size: cover}}</style>""",unsafe_allow_html=True)
 
     def initialize_ui_sidebar(self):
-        with self.st_1.sidebar:    
+        with self.st_obj.sidebar:    
 
-           self.st_1.sidebar.image("resources/DARPG_Logo.jpg", use_column_width=True)
+           self.st_obj.sidebar.image("resources/DARPG_Logo.jpg", use_column_width=True)
            "**Welcome to DARPG Chatbot**"
-           self.st_1.text_area(label="**Instructions**",value="1. This chatbot can answer your questions related Public Grievance\n2. If you know the department, select it from the dropdown and ask questions related to the department\n3. If you do not know, leave the department as it is and you can ask any general queries about Grievance redressal process", height=130)
-           self.st_1.text_area(label="**Note**",value="The list of ministry/department is provided and when selected will help you with the categories and subcategories of complaints you can raise under the respective ministry/department.\nThis is for informatonal purpose to help you select the righ category/subcategory", height=135)
+           self.st_obj.text_area(label="**Instructions**",value="1. This chatbot can answer your questions related Public Grievances \n2. If you know the department, select it from the dropdown and ask questions related to the department\n3. If you do not know, leave the department as it is and you can ask any general queries about Grievance redressal process", height=130)
+           self.st_obj.text_area(label="**Note**",value="The list of ministry/department is provided and when selected will help you with the categories and subcategories of complaints you can raise under the respective ministry/department.\nThis is for informational purpose to help you select the righ category/subcategory", height=135)
     
     def initialize_ui_categories(self):
         df = pd.read_csv('data/Complaint_Category.csv')
@@ -54,14 +53,14 @@ class grievanceRedressal:
         #print(required_df_1)
 
         # Create the primary dropdown for Category and selected value is assigned to var
-        selected_category = self.st_1.selectbox("Select Department/Ministry for your Queries", required_df_1['Category'].unique())
+        selected_category = self.st_obj.selectbox("Select Department/Ministry for your Queries", required_df_1['Category'].unique())
         
         # Filter the dataframe based on the selected category
         filtered_df = required_df[required_df['ParentCategory'] == selected_category]
         selected_org_code=filtered_df["OrgCode"].unique()[0]
         #print(selected_org_code)
         self.department=selected_category
-        selected_sub_category=self.st_1.selectbox("Complaint categories under "+self.department, filtered_df['Category'].unique())
+        selected_sub_category=self.st_obj.selectbox("Complaint categories under "+self.department, filtered_df['Category'].unique())
         #print(selected_sub_category)
         sub_category_df=required_df[(required_df['ParentCategory']==selected_sub_category) &(required_df["Category"]!=selected_sub_category) & (required_df["OrgCode"]==selected_org_code)]
         sub_category_df=sub_category_df[["Category","ParentCategory"]]
@@ -74,13 +73,13 @@ class grievanceRedressal:
         AgGrid(sub_category_df, gridOptions=grid_options, data_editor=dependent_dropdown_options, height=150,fit_columns_on_grid_load=True )
 
     def get_input_query(self):
-        user_question = self.st_1.text_input("**How can I help you today?**")
+        user_question = self.st_obj.text_input("**How can I help you today?**")
         
         self.inp_query=user_question
 
     def initialize_ui(self):
-        self.st_1.set_page_config("DARPG Chatbot")
-        self.st_1.header("Dept of Adminstrative Reforms & Public Grievances Chatbot")
+        self.st_obj.set_page_config("DARPG Chatbot")
+        self.st_obj.header("Dept of Adminstrative Reforms & Public Grievances Chatbot")
         self.add_bg_from_local('resources/flag.png') 
         self.initialize_ui_categories()
         self.initialize_ui_sidebar()
@@ -89,7 +88,7 @@ class grievanceRedressal:
         if self.department !='All':
             prompt = PromptTemplate(input_variables=["concept"], template="Answer to the ask {concept} from "+self.department+" Department")
         else:
-            prompt = PromptTemplate(input_variables=["concept"], template="Answer the {concept} from all DARPG Department")
+            prompt = PromptTemplate(input_variables=["concept"], template="Answer the {concept} from all DARPG Departments")
      
     
         llm = ChatGoogleGenerativeAI(model="gemini-pro",temperature=0.0)
@@ -115,21 +114,21 @@ class grievanceRedressal:
         return output["text"]
 
     def process(self):
-        if self.st_1.button("Retreive Information"):
+        if self.st_obj.button("Retreive Information"):
 
-            with self.st_1.spinner("Retrieving Information..."):
+            with self.st_obj.spinner("Retrieving Information..."):
                 llm_resp = self.return_out()
-                st.session_state['chat_history'].append(("Bot", llm_resp))
-                self.st_1.write(llm_resp)
+                self.st_obj.session_state['chat_history'].append(("Bot", llm_resp))
+                self.st_obj.write(llm_resp)
 
-                st.subheader("The Chat History is")
+                self.st_obj.subheader("The Chat History is")
                 for role, text in st.session_state['chat_history']:
-                    st.write(f"{role}: {text}") 
+                    self.st_obj.write(f"{role}: {text}") 
         
 
 def main():
     initialize_ai_env()
-    grievanceRedressal_obj=grievanceRedressal(st,'','')
+    grievanceRedressal_obj=grievanceRedressal(st)
     grievanceRedressal_obj.initialize_ui()
     grievanceRedressal_obj.get_input_query()
     grievanceRedressal_obj.process()
